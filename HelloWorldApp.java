5353e8e631fa568247588cc03c7d300a011db152
adityashukla44 <adityashukla32550@gmail.com>
2026-03-26 08:57:34 +0530

    feat(UC7): Use String.join() to concatenate names with delimiter

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,11 +3,7 @@ public class HelloWorldApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String names = "";
-            for (String name : args) {
-                names = names + name + ", ";
-            }
-            names = names.substring(0, names.length() - 2);
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
